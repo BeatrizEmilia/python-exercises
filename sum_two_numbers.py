4245814fdbc82475934c3719b69f8b3fbac11baf
Beatriz Emilia <beatrizemilia89@gmail.com>
2026-03-05 10:56:31 -0300

    Create sum_two_numbers.py

---
+++ sum_two_numbers.py
@@ -0,0 +1,8 @@
+# Program that reads two numbers and shows their sum
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+soma = n1 + n2
+
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
