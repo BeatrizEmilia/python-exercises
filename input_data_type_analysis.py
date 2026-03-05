7415054be397b0b6654d6b1f03f07cff00c40aaa
Beatriz Emilia <beatrizemilia89@gmail.com>
2026-03-05 11:27:41 -0300

    Create input_data_type_analysis.py
    Program that reads a value entered by the user and analyzes its characteristics, such as whether it is numeric, alphabetic, uppercase, lowercase, or contains only spaces.

---
+++ input_data_type_analysis.py
@@ -0,0 +1,11 @@
+# Program that reads user input and analyzes its characteristics
+
+a = input('Digite algo: ')
+
+print('O tipo primitivo desse valor é:', type(a))
+print('Só tem espaço?', a.isspace())
+print('É um número?', a.isnumeric())
+print('É alfabético?', a.isalpha())
+print('Está em maiúscula?', a.isupper())
+print('Está em minúscula?', a.islower())
+print('Está capitalizada?', a.istitle())
