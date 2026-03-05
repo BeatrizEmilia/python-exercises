8a97a10a795a165663959e13e8d1716741c9ce36
Beatriz Emilia <beatrizemilia89@gmail.com>
2026-03-05 18:17:24 -0300

    Create predecessor_successor.py
    Reads an integer and displays its predecessor and successor.

---
+++ predecessor_successor.py
@@ -0,0 +1,13 @@
+# Predecessor and Successor
+# This program reads an integer and displays its predecessor and successor.
+
+# User input 
+number = int(input('Enter an integer: '))
+
+# Calculation 
+predecessor = number - 1
+successor = number + 1
+
+# Display result 
+print(f'The predecessor and successor of {number} are {predecessor} and {successor})
+    
